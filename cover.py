e9e22a3b67e178e35a496637b0ad65abc03d49fe
fox <oldDriver@163.com>
2021-11-18 10:08:55 +0800

    下载封面不使用xpath,使用正则

--- cover.py
+++ cover.py
@@ -3,16 +3,17 @@
 
 import requests
 import os
-from lxml import etree
+import re
 
 def downloadCover(htmlfile,folderPath):
     print('开始下载封面')
 
-    html = etree.HTML(htmlfile.text)
-    imageUrl = html.xpath('//meta[@property="og:image"]/@content')[0]
+    # html = etree.HTML(htmlfile.text)
+    # imageUrl = html.xpath('//meta[@property="og:image"]/@content')[0]
+    imageUrl = re.findall("<meta property=\"og:image\" content=\"(.+?)\"", htmlfile.text)[0]
     respone = requests.get(imageUrl)
     fileName = folderPath.split(os.path.sep)[-1]
-    with open(os.path.join(folderPath,fileName + '.jpg'),"wb")as f:
+    with open(os.path.join(folderPath,fileName + '.jpg'),"wb") as f:
         f.write(respone.content)
         
     print('下载封面完成')
